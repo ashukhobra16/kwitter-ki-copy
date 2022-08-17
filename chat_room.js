e2a7192da88ffa77371b9012469008d07a52b256
ashukhobra16 <88964023+ashukhobra16@users.noreply.github.com>
2022-08-17 20:20:59 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -1,10 +1,18 @@
-
-
-
-
-
-
-
+const config = {
+   apiKey: "AIzaSyB9KiYtNtSK_XJFJ_F-_YoRiD9xDmhXCCE",
+   authDomain: "chatt-b3c93.firebaseapp.com",
+   databaseURL: "https://chatt-b3c93-default-rtdb.firebaseio.com",
+   projectId: "chatt-b3c93",
+   storageBucket: "chatt-b3c93.appspot.com",
+   messagingSenderId: "961444473049",
+   appId: "1:961444473049:web:1e9750ebf9c255d889f6c8",
+   measurementId: "G-N58WYMH3BC"
+ };
+ 
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "WELCOME " + user_name + "!";
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
